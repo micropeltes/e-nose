bc0cc8ee62b8bca10668882d6165b27657cd21c2
msi pc <kristoforusgwayne@gmail.com>
2025-11-16 23:19:20 +0700

    basecode

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,46 @@
 #include <Arduino.h>
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
 
-// put function declarations here:
-int myFunction(int, int);
+LiquidCrystal_I2C lcd(0x27,20,4);
+
+// Variabel penyimpanan nilai ADC
+int mq2 = 0;     // P34 = ADC1_CH6
+int mq7 = 0;     // P35 = ADC1_CH7
+int mq135 = 0;   // P25 = ADC1_CH8
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  lcd.init();
+  lcd.backlight();
+
+  // Set pin sebagai input ADC (opsional karena analogRead otomatis)
+  pinMode(34, INPUT);
+  pinMode(35, INPUT);
+  pinMode(25, INPUT);
+
+  lcd.setCursor(0,0);
+  lcd.print("ADC Reader Ready");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  // Baca nilai ADC
+  mq2 = analogRead(34);    // ADC1_CH6
+  mq7 = analogRead(35);    // ADC1_CH7
+  mq135 = analogRead(25);  // ADC1_CH8
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  // Tampilkan ke LCD
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("MQ2 : ");
+  lcd.print(mq2);
+
+  lcd.setCursor(0,1);
+  lcd.print("MQ7 : ");
+  lcd.print(mq7);
+
+  lcd.setCursor(0,2);
+  lcd.print("MQ135: ");
+  lcd.print(mq135);
+
+  delay(500); // jeda 0.5 detik
 }
