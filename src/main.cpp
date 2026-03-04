c83f9a19614a03f804fc184dcc469ebc9a2c2d56
micropeltes <kristoforusgwayne@gmail.com>
2026-03-04 18:36:17 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,30 @@
 #include <Arduino.h>
 #include <Wire.h>
 #include <Adafruit_ADS1X15.h>
+#include <LiquidCrystal_I2C.h>
 
 Adafruit_ADS1115 ads;
 
+// alamat LCD 0x27, ukuran 16 kolom 2 baris
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
 void setup() {
   Serial.begin(115200);
-  ads.begin();
+  Wire.begin();
+
+  lcd.init();
+  lcd.backlight();
+
+  if (!ads.begin()) {
+    lcd.setCursor(0,0);
+    lcd.print("ADS1115 ERROR");
+    while (1);
+  }
+
+  lcd.setCursor(0,0);
+  lcd.print("ADS1115 Ready");
+  delay(2000);
+  lcd.clear();
 }
 
 void loop() {
@@ -15,18 +33,31 @@ void loop() {
   int16_t adc2;
   int16_t adc3;
 
-  adc0 = ads.readADC_SingleEnded(0);
-  adc1 = ads.readADC_SingleEnded(1);
-  adc2 = ads.readADC_SingleEnded(2);
-  adc3 = ads.readADC_SingleEnded(3);
+  adc0 = ads.readADC_SingleEnded(0)+7;
+  adc1 = ads.readADC_SingleEnded(1)+7;
+  adc2 = ads.readADC_SingleEnded(2)+7;
+  adc3 = ads.readADC_SingleEnded(3)+7;
 
-  Serial.print("A0: ");
-  Serial.println(adc0);
-  Serial.print("A1: ");
-  Serial.println(adc1);
-    Serial.print("A2: ");
-  Serial.println(adc2);
-    Serial.print("A3: ");
-  Serial.println(adc3);
+  // tampil ke serial
+  Serial.print("A0: "); Serial.println(adc0);
+  Serial.print("A1: "); Serial.println(adc1);
+  Serial.print("A2: "); Serial.println(adc2);
+  Serial.print("A3: "); Serial.println(adc3);
+
+  // tampil ke LCD
+  lcd.clear();
+
+  lcd.setCursor(0,0);
+  lcd.print("A0:");
+  lcd.print(adc0);
+  lcd.print(" A1:");
+  lcd.print(adc1);
+
+  lcd.setCursor(0,1);
+  lcd.print("A2:");
+  lcd.print(adc2);
+  lcd.print(" A3:");
+  lcd.print(adc3);
+
   delay(1000);
 }
