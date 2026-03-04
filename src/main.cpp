4133bbc1ea86cea19f70b27d5f3ef2762dff8422
micropeltes <kristoforusgwayne@gmail.com>
2026-03-04 18:05:54 +0700

    ADC mentah

--- src/main.cpp
+++ src/main.cpp
@@ -1,138 +1,11 @@
 #include <Arduino.h>
-#include <WiFi.h>
-#include <PubSubClient.h>
 #include <Wire.h>
-#include <LiquidCrystal_I2C.h>
 #include <Adafruit_ADS1X15.h>
 
-// ===== I2C CONFIG =====
-//ESP32C3
-#define I2C_SDA  8
-#define I2C_SCL  9
-
-//esp32S
-// #define I2C_SDA  21
-// #define I2C_SCL  22
 Adafruit_ADS1115 ads;
 
-LiquidCrystal_I2C lcd(0x27,16,2);
-
-const char* ssid = "POCO X7 Pro";
-const char* password = "1234567i";
-const char* mqtt_server = "broker.emqx.io";
-
-WiFiClient espClient;
-PubSubClient client(espClient);
-
-// Sensor Values
-int adc_mq135 = 0;
-int adc_mems  = 0;
-
-float Vadc_mq135, Vsensor_mq135, Rs_mq135;
-float Vadc_mems;
-
-// FreeRTOS Tasks
-TaskHandle_t TaskSensorHandle;
-TaskHandle_t TaskLCDHandle;
-TaskHandle_t TaskMQTTHandle;
-
-
-// MQTT Callback
-void callback(char* topic, byte* payload, unsigned int length) {
-  Serial.print("Topic: ");
-  Serial.println(topic);
-}
-
-
-// Reconnect MQTT
-void reconnectMQTT() {
-  while (!client.connected()) {
-    Serial.println("MQTT reconnecting...");
-    if (client.connect("ESP32_CLIENT_1")) {
-      client.subscribe("sensor/control");
-      Serial.println("MQTT Connected!");
-    }
-    delay(500);
-  }
-}
-
-
-// Task Sensor (Core 0)
-void TaskSensor(void * parameter) {
-
-  const float RLOAD = 3000.0;
-  const float VCC = 4.096;  // sesuai GAIN_ONE
-
-  for (;;) {
-
-    int16_t raw_h2s  = ads.readADC_SingleEnded(0);
-    int16_t raw_mq135 = ads.readADC_SingleEnded(1);
-    int16_t raw_nh3  = ads.readADC_SingleEnded(2);
-
-    float V_h2s  = ads.computeVolts(raw_h2s);
-    float V_mq135 = ads.computeVolts(raw_mq135);
-    float V_nh3  = ads.computeVolts(raw_nh3);
-
-    // Hitung Rs MQ135
-    Rs_mq135 = RLOAD * ((VCC / V_mq135) - 1.0);
-    float Rs_kohm = Rs_mq135 / 1000.0;
-
-    Serial.printf(
-      "H2S: %.3fV | MQ135: %.3fV Rs: %.2fkΩ | NH3: %.3fV\n",
-      V_h2s, V_mq135, Rs_kohm, V_nh3
-    );
-
-    vTaskDelay(500 / portTICK_PERIOD_MS);
-  }
-}
-
-
-// Task LCD (Core 1)
-void TaskLCD(void * parameter) {
-  lcd.init();
-  lcd.backlight();
-
-  for (;;) {
-    lcd.setCursor(0,0); 
-    lcd.print("MQ Rs: ");
-    lcd.print(Rs_mq135, 1);
-    lcd.print("k ");
-
-    lcd.setCursor(0,1);
-    lcd.print("MEMS: ");
-    lcd.print(Vadc_mems, 2);
-    lcd.print("V  ");
-
-    vTaskDelay(800 / portTICK_PERIOD_MS);
-  }
-}
-
-
-// Task MQTT (Core 1)
-void TaskMQTT(void * parameter) {
-  for (;;) {
-    if (!client.connected()) {
-      reconnectMQTT();
-    }
-
-    client.loop();
-
-    String payload = "{\"MQ135_ADC\":"+String(adc_mq135)+
-                     ",\"MQ135_Rs\":"+String(Rs_mq135,2)+
-                     ",\"MEMS_ADC\":"+String(adc_mems)+
-                     ",\"MEMS_V\":"+String(Vadc_mems,2)+"}";
-
-    client.publish("sensor/gas", payload.c_str());
-
-    Serial.println("MQTT Payload: " + payload);
-
-    vTaskDelay(1000 / portTICK_PERIOD_MS);
-  }
-}
-
-
-// SETUP
 void setup() {
+<<<<<<< Updated upstream
   Serial.begin(9600);
   Wire.begin(I2C_SDA, I2C_SCL);
   
@@ -188,6 +61,30 @@ void setup() {
       1,
       &TaskMQTTHandle
   );
+=======
+  Serial.begin(115200);
+  ads.begin();
+>>>>>>> Stashed changes
 }
 
-void loop() {}
+void loop() {
+  int16_t adc0;
+  int16_t adc1;
+  int16_t adc2;
+  int16_t adc3;
+
+  adc0 = ads.readADC_SingleEnded(0);
+  adc1 = ads.readADC_SingleEnded(1);
+  adc2 = ads.readADC_SingleEnded(2);
+  adc3 = ads.readADC_SingleEnded(3);
+
+  Serial.print("A0: ");
+  Serial.println(adc0+7);
+  Serial.print("A1: ");
+  Serial.println(adc1+7);
+    Serial.print("A2: ");
+  Serial.println(adc2+7);
+    Serial.print("A3: ");
+  Serial.println(adc3+7);
+  delay(1000);
+}
