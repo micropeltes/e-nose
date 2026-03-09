7f46bb46427d450cfa0fd12d735bcf42d90625b2
micropeltes <kristoforusgwayne@gmail.com>
2026-03-10 00:39:24 +0700

    adadada

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,10 @@
 #include <Adafruit_ADS1X15.h>
 #include <LiquidCrystal_I2C.h>
 
-Adafruit_ADS1115 ads;
-
-// alamat LCD 0x27, ukuran 16 kolom 2 baris
+// I2C defines
+Adafruit_ADS1115 ads1;
+Adafruit_ADS1115 ads2;
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 void setup() {
@@ -15,49 +16,48 @@ void setup() {
   lcd.init();
   lcd.backlight();
 
-  if (!ads.begin()) {
+  // ADS1 ADDR -> GND mq135 dan mems h2s
+  lcd.setCursor(0,0);
+  if (!ads1.begin(0x48)) {
     lcd.setCursor(0,0);
-    lcd.print("ADS1115 ERROR");
+    lcd.print("ADS1 gagal");
+    while (1);
+  }
+  else{
+
+  }
+
+  // ADS2 ADDR -> SDA mems nh3 dan mics 6814
+  if (!ads2.begin(0x4A)) {
+    lcd.setCursor(0,1);
+    lcd.print("ADS2 gagal");
     while (1);
   }
 
   lcd.setCursor(0,0);
   lcd.print("ADS1115 Ready");
   delay(2000);
-  lcd.clear();
+  lcd.clear();    
 }
 
 void loop() {
-  int16_t adc0;
-  int16_t adc1;
-  int16_t adc2;
-  int16_t adc3;
 
-  adc0 = ads.readADC_SingleEnded(0)+7;
-  adc1 = ads.readADC_SingleEnded(1)+7;
-  adc2 = ads.readADC_SingleEnded(2)+7;
-  adc3 = ads.readADC_SingleEnded(3)+7;
+  // ADS1
+  int16_t adc1_0 = ads1.readADC_SingleEnded(0);
+  int16_t adc1_1 = ads1.readADC_SingleEnded(1);
+  int16_t adc1_2 = ads1.readADC_SingleEnded(2);
+  int16_t adc1_3 = ads1.readADC_SingleEnded(3);
+  // ADS2
+  int16_t adc2_0 = ads2.readADC_SingleEnded(0);
+  int16_t adc2_1 = ads2.readADC_SingleEnded(1);
+  int16_t adc2_2 = ads2.readADC_SingleEnded(2);
+  int16_t adc2_3 = ads2.readADC_SingleEnded(3);
 
-  // tampil ke serial
-  Serial.print("A0: "); Serial.println(adc0);
-  Serial.print("A1: "); Serial.println(adc1);
-  Serial.print("A2: "); Serial.println(adc2);
-  Serial.print("A3: "); Serial.println(adc3);
+  Serial.print("ADS1 CH0: "); Serial.print(adc1_0);
+  Serial.print(" | CH1: "); Serial.print(adc1_0);
 
-  // tampil ke LCD
-  lcd.clear();
-
-  lcd.setCursor(0,0);
-  lcd.print("A0:");
-  lcd.print(adc0);
-  lcd.print(" A1:");
-  lcd.print(adc1);
-
-  lcd.setCursor(0,1);
-  lcd.print("A2:");
-  lcd.print(adc2);
-  lcd.print(" A3:");
-  lcd.print(adc3);
+  Serial.print(" || ADS2 CH0: "); Serial.print(adc1_0);
+  Serial.print(" | CH1: "); Serial.println(adc1_0);
 
   delay(1000);
 }
