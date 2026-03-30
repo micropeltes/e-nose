277c945a22cce24de38cdc08ca2988a3b73a82b1
micropeltes <kristoforusgwayne@gmail.com>
2026-03-30 22:05:57 +0700

    untested stecu stelan cuek malu malu

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,39 @@
 #include <Arduino.h>
 #include <Wire.h>
+<<<<<<< Updated upstream
 #include <WiFi.h>
 #include <PubSubClient.h>
+=======
+>>>>>>> Stashed changes
 #include <Adafruit_ADS1X15.h>
 #include <LiquidCrystal_I2C.h>
+#include <WiFi.h>
+#include <WiFiClientSecure.h>
+#include <PubSubClient.h>
+#include "mqtt_ca_cert.h"
 
+<<<<<<< Updated upstream
 // I2C
 Adafruit_ADS1115 ads1;
 Adafruit_ADS1115 ads2;
+=======
+// I2C defines
+Adafruit_ADS1115 ads1;
+Adafruit_ADS1115 ads2;
+LiquidCrystal_I2C lcd1(0x27, 16, 2);
+LiquidCrystal_I2C lcd2(0x20, 16, 2);
+
+// ADS1
+int16_t adc1_0 = ads1.readADC_SingleEnded(0);
+int16_t adc1_1 = ads1.readADC_SingleEnded(1);
+int16_t adc1_2 = ads1.readADC_SingleEnded(2);
+int16_t adc1_3 = ads1.readADC_SingleEnded(3);
+// ADS2
+int16_t adc2_0 = ads2.readADC_SingleEnded(0);
+int16_t adc2_1 = ads2.readADC_SingleEnded(1);
+int16_t adc2_2 = ads2.readADC_SingleEnded(2);
+int16_t adc2_3 = ads2.readADC_SingleEnded(3);
+>>>>>>> Stashed changes
 
 LiquidCrystal_I2C lcd1(0x27, 16, 2);
 LiquidCrystal_I2C lcd2(0x20, 16, 2);
@@ -19,11 +45,14 @@ int16_t adc2_0, adc2_1, adc2_2, adc2_3;
 // WIFI + MQTT
 const char* ssid = "POCO X7 Pro";
 const char* password = "1234567i";
-const char* mqtt_server = "broker.emqx.io";
+const char* mqtt_server = "45.126.43.35";
+const uint16_t mqtt_port = 1199;
 
-WiFiClient espClient;
+WiFiClientSecure espClient;
 PubSubClient client(espClient);
+TaskHandle_t TaskMQTTHandle = NULL;
 
+<<<<<<< Updated upstream
 TaskHandle_t TaskSensorHandle;
 TaskHandle_t TaskMQTTHandle;
 TaskHandle_t TaskLCDHandle;
@@ -42,6 +71,15 @@ void connectWiFi() {
 
   while (WiFi.status() != WL_CONNECTED) {
     Serial.print(".");
+=======
+void reconnectMQTT() {
+  while (!client.connected()) {
+    Serial.println("MQTT reconnecting...");
+    if (client.connect("ESP32_CLIENT_1")) {
+      client.subscribe("sensor/control");
+      Serial.println("MQTT Connected!");
+    }
+>>>>>>> Stashed changes
     delay(500);
   }
 
@@ -70,6 +108,7 @@ void reconnectMQTT() {
 
 // SENSOR TASK
 void TaskSensor(void * parameter) {
+<<<<<<< Updated upstream
 
   for (;;) {
 
@@ -83,11 +122,18 @@ void TaskSensor(void * parameter) {
     adc2_2 = ads2.readADC_SingleEnded(2);
     adc2_3 = ads2.readADC_SingleEnded(3);
 
+=======
+  
+  for (;;) {
+>>>>>>> Stashed changes
     vTaskDelay(500 / portTICK_PERIOD_MS);
   }
 }
 
+<<<<<<< Updated upstream
 // MQTT TASK
+=======
+>>>>>>> Stashed changes
 void TaskMQTT(void * parameter) {
 
   for (;;) {
@@ -98,6 +144,7 @@ void TaskMQTT(void * parameter) {
 
     client.loop();
 
+<<<<<<< Updated upstream
     String payload = "{"
     "\"adc1\":"+String(adc1_0)+
     ",\"adc2\":"+String(adc1_1)+
@@ -108,6 +155,9 @@ void TaskMQTT(void * parameter) {
     ",\"adc7\":"+String(adc2_2)+
     ",\"adc8\":"+String(adc2_3)+
     "}";
+=======
+    String payload = "{\"MQ135_ADC\"}";
+>>>>>>> Stashed changes
 
     client.publish("sensor/gas", payload.c_str());
 
@@ -117,6 +167,7 @@ void TaskMQTT(void * parameter) {
   }
 }
 
+<<<<<<< Updated upstream
 // LCD TASK
 void TaskLCD(void * parameter) {
 
@@ -182,10 +233,41 @@ void setup() {
   }
 
   connectWiFi();
+=======
+void setup() {
+  Serial.begin(115200);
+  Wire.begin();
+  espClient.setCACert(MQTT_CA_CERT);
+  client.setServer(mqtt_server, mqtt_port);
 
-  client.setServer(mqtt_server, 1883);
-  client.setCallback(callback);
+  lcd1.init();
+  lcd1.backlight();
 
+  // ADS1 ADDR -> GND mq135 dan mems h2s
+  lcd1.setCursor(0,0);
+  if (!ads1.begin(0x48)) {
+    lcd1.setCursor(0,0);
+    lcd1.print("ADS1 gagal");
+    while (1);
+  }
+  else{
+>>>>>>> Stashed changes
+
+  }
+
+  // ADS2 ADDR -> SDA mems nh3 dan mics 6814
+  if (!ads2.begin(0x4A)) {
+    lcd1.setCursor(0,1);
+    lcd1.print("ADS2 gagal");
+    while (1);
+  }
+
+  lcd1.setCursor(0,0);
+  lcd1.print("ADS1115 Ready");
+  delay(2000);
+  lcd1.clear();
+
+<<<<<<< Updated upstream
   // CREATE TASKS
   xTaskCreate(
     TaskSensor,
@@ -216,4 +298,17 @@ void setup() {
 }
 
 void loop() {
+=======
+  xTaskCreatePinnedToCore(TaskMQTT, "TaskMQTT", 4096, NULL, 1, &TaskMQTTHandle, 1);
+}
+
+void loop() {
+  Serial.print("ADS1 CH0: "); Serial.print(adc1_0);
+  Serial.print(" | CH1: "); Serial.print(adc1_0);
+
+  Serial.print(" || ADS2 CH0: "); Serial.print(adc1_0);
+  Serial.print(" | CH1: "); Serial.println(adc1_0);
+
+  delay(1000);
+>>>>>>> Stashed changes
 }
